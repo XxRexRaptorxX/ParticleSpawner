72831137b467992908f37b8994eead18edbb1d48
XxRexRaptorxX <Raptor-Simon@web.de>
2022-05-19 20:18:45 +0200

    blockstate errors fixed

--- src/main/java/xxrexraptorxx/particle_spawner/blocks/ParticleBlock.java
+++ src/main/java/xxrexraptorxx/particle_spawner/blocks/ParticleBlock.java
@@ -71,10 +71,10 @@ public class ParticleBlock extends Block implements SimpleWaterloggedBlock {
 			int strength = state.getValue(PARTICLE_STRENGTH) + strengthChange;
 			int range = state.getValue(PARTICLE_RANGE) + rangeChange;
 
-			//test if new value is higher than the max value, and reset it if to high
-			if (type > Config.PARTICLE_SPAWNER_TYPE_MAX_VALUE.get()) 			type = type - Config.PARTICLE_SPAWNER_TYPE_MAX_VALUE.get() + 1;
-			if (strength > Config.PARTICLE_SPAWNER_STRENGTH_MAX_VALUE.get()) 	strength = strength - Config.PARTICLE_SPAWNER_STRENGTH_MAX_VALUE.get() + 1;
-			if (range > Config.PARTICLE_SPAWNER_RANGE_MAX_VALUE.get()) 			range = range - Config.PARTICLE_SPAWNER_RANGE_MAX_VALUE.get() + 1;
+			//test if new value is higher than the max value or 0, and reset it if to high
+			if (type > Config.PARTICLE_SPAWNER_TYPE_MAX_VALUE.get() || type == 0) 				type = 1;
+			if (strength > Config.PARTICLE_SPAWNER_STRENGTH_MAX_VALUE.get() || strength == 0) 	strength = 1;
+			if (range > Config.PARTICLE_SPAWNER_RANGE_MAX_VALUE.get() || range == 0) 			range = 1;
 
 			//update the block with the new values
 			level.setBlock(pos, ModBlocks.PARTICLE.get().defaultBlockState().setValue(ParticleBlock.POWERED, true).setValue(ParticleBlock.PARTICLE_TYPE, type).setValue(ParticleBlock.PARTICLE_STRENGTH, strength).setValue(ParticleBlock.PARTICLE_RANGE, range).setValue(ParticleBlock.WATERLOGGED, Boolean.valueOf(fluidstate.getType() == Fluids.WATER)), 11);
