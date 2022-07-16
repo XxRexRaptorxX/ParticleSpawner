79d3cae409f483eaa2477e61ea0fba69a4dec12a
XxRexRaptorxX <Raptor-Simon@web.de>
2022-07-16 18:40:05 +0200

    Patreon integration + improved version checker

--- src/main/java/xxrexraptorxx/particle_spawner/world/Events.java
+++ src/main/java/xxrexraptorxx/particle_spawner/world/Events.java
@@ -3,17 +3,25 @@ package xxrexraptorxx.particle_spawner.world;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.ClickEvent;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.stats.Stats;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.Fluids;
 import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModList;
@@ -26,8 +34,14 @@ import xxrexraptorxx.particle_spawner.main.ParticleSpawner;
 import xxrexraptorxx.particle_spawner.main.References;
 import xxrexraptorxx.particle_spawner.utils.Config;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+import java.util.Scanner;
+
 @Mod.EventBusSubscriber(modid = References.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class Events {
+
 
 
     /** Update-Checker **/
@@ -40,8 +54,11 @@ public class Events {
                 if (VersionChecker.getResult(ModList.get().getModContainerById(References.MODID).get().getModInfo()).status() == VersionChecker.Status.OUTDATED ||
                         VersionChecker.getResult(ModList.get().getModContainerById(References.MODID).get().getModInfo()).status() == VersionChecker.Status.BETA_OUTDATED ) {
 
-                    Minecraft.getInstance().player.sendSystemMessage(Component.literal(ChatFormatting.BLUE + "A newer version of " + ChatFormatting.YELLOW + References.NAME + ChatFormatting.BLUE + " is available!"));
-                    Minecraft.getInstance().player.sendSystemMessage(Component.literal(ChatFormatting.GRAY + References.URL));
+                    MutableComponent url = Component.literal(ChatFormatting.GREEN + "Click here to update!");
+                    url.withStyle(url.getStyle().withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, References.URL)));
+
+                    Minecraft.getInstance().player.displayClientMessage(Component.literal(ChatFormatting.BLUE + "A newer version of " + ChatFormatting.YELLOW + References.NAME + ChatFormatting.BLUE + " is available!"), false);
+                    Minecraft.getInstance().player.displayClientMessage(url, false);
 
                     hasShownUp = true;
 
@@ -114,6 +131,96 @@ public class Events {
                 }
             }
         }
+    }
+
+
+
+    /**
+     * Distributes the supporter rewards on first join.
+     */
+    @SubscribeEvent
+    public static void SupporterRewards(PlayerEvent.PlayerLoggedInEvent event) {
+        Player player = event.getPlayer();
+        Level world = player.getLevel();
+
+        if (Config.PATREON_REWARDS.get()) {
+
+            try {
+                URL SUPPORTER_URL = new URL("https://raw.githubusercontent.com/XxRexRaptorxX/Patreons/main/Supporter");
+                URL PREMIUM_SUPPORTER_URL = new URL("https://raw.githubusercontent.com/XxRexRaptorxX/Patreons/main/Premium%20Supporter");
+                URL ELITE_URL = new URL("https://raw.githubusercontent.com/XxRexRaptorxX/Patreons/main/Elite");
+
+                //test if a player already has rewards
+                if (!player.getInventory().contains(new ItemStack(Items.PAPER))) {
+
+                    ServerPlayer serverPlayer = (ServerPlayer) player;
+                    //test if player joins the first time
+                    if (serverPlayer.getStats().getValue(Stats.CUSTOM, Stats.PLAY_TIME) < 5) {
+
+                        //test if player is supporter
+                        if (SupporterCheck(SUPPORTER_URL, player)) {
+
+                            ItemStack certificate = new ItemStack(Items.PAPER).setHoverName((Component.literal("Thank you for supporting me in my work!").withStyle(ChatFormatting.GOLD).append(Component.literal(" - XxRexRaptorxX").withStyle(ChatFormatting.ITALIC).withStyle(ChatFormatting.GREEN))));
+
+                            CompoundTag ownerNBT = new CompoundTag();
+                            ItemStack reward = new ItemStack(Items.PLAYER_HEAD);
+                            ownerNBT.putString("SkullOwner", player.getName().getString());
+                            reward.setTag(ownerNBT);
+
+                            player.getLevel().playSound((Player) null, player.blockPosition(), SoundEvents.PLAYER_LEVELUP, SoundSource.PLAYERS, 0.5F, world.random.nextFloat() * 0.15F + 0.8F);
+                            player.addItem(reward);
+                            player.addItem(certificate);
+                        }
+
+                        //test if player is premium supporter
+                        if (SupporterCheck(PREMIUM_SUPPORTER_URL, player)) {
+                            ItemStack reward = new ItemStack(Items.DIAMOND_SWORD, 1).setHoverName(Component.literal("Rex's Night Sword").withStyle(ChatFormatting.DARK_GRAY));
+                            reward.enchant(Enchantments.MENDING, 1);
+                            reward.enchant(Enchantments.SHARPNESS, 3);
+                            player.addItem(reward);
+                        }
+
+                        //test if player is elite
+                        if (SupporterCheck(ELITE_URL, player)) {
+                            player.addItem(new ItemStack(Items.NETHER_STAR).setHoverName(Component.literal("Elite Star")));
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Tests if a player is a supporter
+     *
+     * @param url url to a file that contains the supporter names
+     * @param player ingame player
+     * @return true/false
+     */
+    private static boolean SupporterCheck(URL url, Player player) {
+        try {
+            Scanner scanner = new Scanner(url.openStream());
+            List<String> supporterList = scanner.tokens().toList();
+
+            for (String name: supporterList) {
+                //test if player is in supporter list
+                if (player.getName().getString().equals(name)) {
+                    return true;
+                }
+            }
+
+            scanner.close();
+
+        } catch (MalformedURLException e) {
+            ParticleSpawner.LOGGER.error("Supporter list URL not found! >>" + url);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return false;
     }
 
 
